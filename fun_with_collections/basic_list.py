e637f4020ac265b016b274ea7a3ef769d3518489
applicationsbypaul <32409508+applicationsbypaul@users.noreply.github.com>
2020-06-21 18:21:11 -0500

    completed the assignment test past and the make list actaully is how it should be.

--- fun_with_collections/basic_list.py
+++ fun_with_collections/basic_list.py
@@ -1,8 +1,38 @@
+"""
+Program: basic_list.py
+Author: Paul Ford
+Last date modified: 06/21/2020
+Purpose: uses inner functions to be able to
+         get a list of numbers from a user
+"""
+
+
 def make_list():
-    # return a list of user input
-    pass
+    """
+    creates a list and checks for valid data.
+    :return: returns a list of 3 integers
+    """
+    a_list = []
+    for index in range(3):
+        while True:
+            try:
+                user_input = int(get_input())
+            except ValueError:
+                print('User must submit a number.')
+                continue
+            a_list.insert(index, user_input)
+            break
+    return a_list
 
 
 def get_input():
-    # get one input and return it
-    pass
+    """
+    Ask the user for a number
+    :return: returns a string of the user input
+    """
+    user_input = input('Please enter a number')
+    return user_input
+
+
+if __name__ == '__main__':
+    print(make_list())
--- test_collections/test_basic_list.py
+++ test_collections/test_basic_list.py
@@ -1,13 +1,21 @@
+"""
+Program: test_basic_list.py
+Author: Paul Ford
+Last date modified: 06/21/2020
+Purpose: uses inner functions to be able to
+         get a list of numbers from a user
+"""
+
 import unittest
 from unittest.mock import patch
 
-from fun_with_collections.basic_list import make_list
+from fun_with_collections.basic_list import make_list, get_input
 
 
 class TestList(unittest.TestCase):
-    @patch('fun_with_collections.topic1.get_input', return_value='5')
+    @patch('fun_with_collections.basic_list.get_input', return_value='5')
     def test_make_list(self, input):
-        self.assertEqual(make_list(), [2, 54, 28])
+        self.assertEqual(make_list(), [5, 5, 5])
 
 
 if __name__ == '__main__':
