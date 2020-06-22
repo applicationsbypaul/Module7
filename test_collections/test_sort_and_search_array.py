9241a3e760778d1cb8ac49f360b79c0418418386
applicationsbypaul <32409508+applicationsbypaul@users.noreply.github.com>
2020-06-21 22:58:03 -0500

    got my first test to pass. I just made the array sorted in the test function then tested my function.

--- test_collections/test_sort_and_search_array.py
+++ test_collections/test_sort_and_search_array.py
@@ -1,11 +1,13 @@
 import unittest
+import array as arr
 
 from fun_with_collections.sort_and_search_array import sort_array, search_array
 
 
 class MyTestCase(unittest.TestCase):
     def test_sort(self):
-        self.assertEqual(sort_array(), False)
+        test_array = arr.array('i', [-4, 2, 3, 45, 50, 66, 99])
+        self.assertEqual(sort_array(), test_array)
 
     def test_search(self):
         self.assertEqual(search_array(), False)
