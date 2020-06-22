67bd91344e8f76c7f77ce047783f042c8c5223ff
applicationsbypaul <32409508+applicationsbypaul@users.noreply.github.com>
2020-06-21 19:09:58 -0500

    Created test for below and above 1 and 50 range..

--- test_collections/test_basic_list_exception.py
+++ test_collections/test_basic_list_exception.py
@@ -17,6 +17,21 @@ class TestList(unittest.TestCase):
     def test_make_list(self, input):
         self.assertEqual(make_list(), [5, 5, 5])
 
+    @patch('fun_with_collections.basic_list_exception.get_input', return_value='ab')  # patch function for input
+    def test_make_list_non_numeric(self, input):  # pass input
+        with self.assertRaises(ValueError):  # this is familiar
+            make_list()  # call the function!
+
+    @patch('fun_with_collections.basic_list_exception.get_input', return_value='-1')
+    def test_make_list_below_range(self):
+        with self.assertRaises(ValueError):
+            make_list()
+
+    @patch('fun_with_collections.basic_list_exception.get_input', return_value='55')
+    def test_make_list_above_range(self):
+        with self.self.assertRaises(ValueError):
+            make_list()
+
 
 if __name__ == '__main__':
     unittest.main()
