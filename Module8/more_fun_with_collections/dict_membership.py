306e9449470389aed0304f59d7dea2e045c0350e
applicationsbypaul <32409508+applicationsbypaul@users.noreply.github.com>
2020-06-22 15:55:02 -0500

    Created test dictionary in test and implemented method. Test pass.

--- Module8/more_fun_with_collections/dict_membership.py
+++ Module8/more_fun_with_collections/dict_membership.py
@@ -7,6 +7,11 @@ Purpose: using dictionaries for the first time
 
 
 def in_dict(dictionary, data):
-    # accept a set and return a boolean value
-    # stating if the element is in the dictionary
-    pass
+    """
+    accept a set and return a boolean value
+    stating if the element is in the dictionary
+    :param dictionary: The given dictionary to search
+    :param data: The data searching in the dictionary
+    :return: A boolean if the data is in the dictionary
+    """
+    return data in dictionary
--- Module8/test/test_dict_membership.py
+++ Module8/test/test_dict_membership.py
@@ -7,14 +7,17 @@ Purpose: test in_dict
 import unittest
 
 from Module8.more_fun_with_collections.dict_membership import in_dict
+# test dictionary
+test_dict = {'A': 90, 'B': 80, 'C': 70, 'D': 60, 'F': 0}
 
 
 class MyTestCase(unittest.TestCase):
+
     def test_in_dict_true(self):
-        self.assertEqual(in_dict(), True)
+        self.assertEqual(in_dict(test_dict, 'A'), True)
 
     def test_in_dict_false(self):
-        self.assertEqual(in_dict(), False)
+        self.assertEqual(in_dict(test_dict, 'G'), False)
 
 
 if __name__ == '__main__':
