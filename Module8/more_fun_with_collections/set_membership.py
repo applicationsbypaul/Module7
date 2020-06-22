039352489e5d31a252fa7cc51c6a908c00c0ae10
applicationsbypaul <32409508+applicationsbypaul@users.noreply.github.com>
2020-06-22 14:53:24 -0500

    Finished implementing method. Works, I had to add another parameter to the method so it knows what to look for in the set.

--- Module8/more_fun_with_collections/set_membership.py
+++ Module8/more_fun_with_collections/set_membership.py
@@ -6,10 +6,15 @@ Purpose: using sets for the first time
 """
 
 
-def in_set(user_set):
-    # accept a set and return a boolean value
-    # stating if the element is in the set
-    pass
+def in_set(user_set, data):
+    """
+    accept a set and return a boolean value
+    stating if the element is in the set
+    :param user_set: set we are searching
+    :param data: the key value we are looking for in the set
+    :return: boolean value if data is in set
+    """
+    return data in user_set
 
 
 if __name__ == '__main__':
--- Module8/test/test_set_membership.py
+++ Module8/test/test_set_membership.py
@@ -11,10 +11,10 @@ from Module8.more_fun_with_collections.set_membership import in_set
 
 class MyTestCase(unittest.TestCase):
     def test_in_set_true(self):
-        self.assertEqual(in_set({1, 2, 3, 4, 5, 6, 7, 8, 9}), True)
+        self.assertEqual(in_set({1, 2, 3, 4, 5, 6, 7, 8, 9}, 1), True)
 
     def test_in_set_false(self):
-        self.assertEqual(in_set({1, 2, 3, 4, 5, 6, 7, 8, 9}), False)
+        self.assertEqual(in_set({1, 2, 3, 4, 5, 6, 7, 8, 9}, 10), False)
 
 
 if __name__ == '__main__':
