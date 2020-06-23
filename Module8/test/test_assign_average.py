ac010bd867d6fc2dea1f6a20d14d63683a459391
applicationsbypaul <32409508+applicationsbypaul@users.noreply.github.com>
2020-06-23 09:26:34 -0500

    2nd test passed, commented out the other test for clarity and will remove them 1 by 1 to verify they pass.

--- Module8/test/test_assign_average.py
+++ Module8/test/test_assign_average.py
@@ -8,19 +8,20 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(switch_average('A'), 'a')
 
     def test_switch_average_B(self):
-        self.assertEqual(switch_average(), 'b')
+        self.assertEqual(switch_average('B'), 'b')
 
-    def test_switch_average_C(self):
-        self.assertEqual(switch_average(), 'c')
 
-    def test_switch_average_D(self):
-        self.assertEqual(switch_average(), 'd')
+# def test_switch_average_C(self):
+#    self.assertEqual(switch_average(), 'c')
 
-    def test_switch_average_E(self):
-        self.assertEqual(switch_average(), 'e')
+# def test_switch_average_D(self):
+#     self.assertEqual(switch_average(), 'd')
 
-    def test_switch_average_F(self):
-        self.assertEqual(switch_average(), -1)
+# def test_switch_average_E(self):
+#     self.assertEqual(switch_average(), 'e')
+
+# def test_switch_average_F(self):
+#    self.assertEqual(switch_average(), -1)
 
 
 if __name__ == '__main__':
