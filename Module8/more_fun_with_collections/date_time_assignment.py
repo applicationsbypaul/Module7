8c45dc2a0e4211b695b2e2cd63b943faf5f76ff4
applicationsbypaul <32409508+applicationsbypaul@users.noreply.github.com>
2020-06-23 12:02:23 -0500

    Test past and docstring created.

--- Module8/more_fun_with_collections/date_time_assignment.py
+++ Module8/more_fun_with_collections/date_time_assignment.py
@@ -1,13 +1,25 @@
-import datetime
+"""
+Program: date_time_assignment.py
+Author: Paul Ford
+Last date modified: 06/23/2020
+Purpose: Gives the half birthday
+"""
+from datetime import datetime, timedelta
 
-recent_birthday = datetime.datetime(2020, 5, 21)
+recent_birthday = datetime(2020, 5, 21)
 # half birthday I looked up to be 182 days
 half_birthday_length = 182
 
 
-def half_birthday(date_time):
-    return date_time
+def half_birthday(birthday):
+    """
+    returns the users half birthday
+    in datetime format
+    :param: birthday is the user birthday in timedate format
+    :return: return the users half birthday
+    """
+    return birthday + timedelta(days=half_birthday_length)
 
 
 if __name__ == '__main__':
-    half_birthday(recent_birthday)
+    print(half_birthday(recent_birthday))
