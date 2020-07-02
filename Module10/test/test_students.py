2a8bfa0294351329cd2e5bf5fc7c765380c82d88
applicationsbypaul <32409508+applicationsbypaul@users.noreply.github.com>
2020-07-02 00:14:27 -0500

    tested initial all attributes all is good.

--- Module10/test/test_students.py
+++ Module10/test/test_students.py
@@ -15,6 +15,14 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(self.student.first_name, 'Paul')
         self.assertEqual(self.student.major, 'Computer Engineering')
 
+    def test_inital_all_attributes(self):
+        student = Student('Ford', 'Paul', 'Computer Engineering', 3.5)  # this is not self.person from setUp, but local
+        assert student.last_name == 'Ford'                               # note no self here on person or assert
+        assert student.first_name == 'Paul'
+        assert student.major == 'Computer Engineering'
+        assert student.gpa == 3.5
+
+
 
 if __name__ == '__main__':
     unittest.main()
