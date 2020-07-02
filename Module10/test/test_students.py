bfe23449b1b55d8be52365ce6d6937843ed7459d
applicationsbypaul <32409508+applicationsbypaul@users.noreply.github.com>
2020-07-02 00:25:13 -0500

    created test for first name error, failed test.

--- Module10/test/test_students.py
+++ Module10/test/test_students.py
@@ -17,7 +17,7 @@ class MyTestCase(unittest.TestCase):
 
     def test_inital_all_attributes(self):
         student = Student('Ford', 'Paul', 'Computer Engineering', 3.5)  # this is not self.person from setUp, but local
-        assert student.last_name == 'Ford'                               # note no self here on person or assert
+        assert student.last_name == 'Ford'  # note no self here on person or assert
         assert student.first_name == 'Paul'
         assert student.major == 'Computer Engineering'
         assert student.gpa == 3.5
@@ -25,6 +25,10 @@ class MyTestCase(unittest.TestCase):
     def test_object_not_created_error_last_name(self):
         with self.assertRaises(ValueError):
             s = Student('123', 'Paul', 'English')
+
+    def test_object_not_created_error_fist_name(self):
+        with self.assertRaises(ValueError):
+            s = Student('Ford', '123', 'English')
 
 
 if __name__ == '__main__':
