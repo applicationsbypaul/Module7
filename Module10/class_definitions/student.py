2a12871ce675a15d863067d82d46a9daab4203f0
applicationsbypaul <32409508+applicationsbypaul@users.noreply.github.com>
2020-07-05 07:00:21 -0500

    cleaned up code and all finished now

--- Module10/class_definitions/student.py
+++ Module10/class_definitions/student.py
@@ -2,7 +2,17 @@ class Student:
     """Student class"""
 
     def __init__(self, lname, fname, major, gpa=''):
+        """
+        constructor to create a student
+        :param lname: last name
+        :param fname: first name
+        :param major: Major of student
+        :param gpa: students gpa on a 4.0 scale
+        """
         name_characters = set("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'-' '")
+        '''
+        Checking values for good input
+        '''
         if not (name_characters.issuperset(lname)):
             raise ValueError
         if not (name_characters.issuperset(fname)):
@@ -13,14 +23,18 @@ class Student:
             raise ValueError
         if not 0 <= gpa <= 4:
             raise ValueError
+
+        '''
+        Setting constructor values
+        '''
         self.last_name = lname
         self.first_name = fname
         self.major = major
         self.gpa = gpa
 
     def __str__(self):
+        """
+        prints student data
+        :return: str of students data
+        """
         return self.last_name + ", " + self.first_name + " has major " + self.major + "with gpa: " + str(self.gpa)
-
-
-# driver
-#s = Student('Ford', 'Paul', 'Eng', 4.5)
