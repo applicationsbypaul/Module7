08760cb42b2cecec7b1733396b2d5d68f2e154ea
applicationsbypaul <32409508+applicationsbypaul@users.noreply.github.com>
2020-07-05 09:53:34 -0500

    Combined person and student.

---
+++ Module10/class_definitions/person.py
@@ -0,0 +1,41 @@
+"""
+Program: Person.py
+Author: Paul Ford
+Last date modified: 07/1/2020
+Purpose: Create a class that uses another object in creation.
+"""
+from datetime import datetime
+
+from Module10.class_definitions.student import Student
+
+
+class Person:
+    """Person class"""
+
+    def __init__(self, student, addy=''):
+        """
+        Constructor to create
+        :param student: student
+        :param addy: address
+        """
+        self.address = addy
+        self.student = student
+
+    def __str__(self):
+        """
+        prints person data
+        :return: str of person data
+        """
+        return student.display() + '\nAddress: ' + self.address
+
+
+# Driver
+start_date = datetime(2018, 9, 1)  # create a datetime to confirm
+student = Student('Ford', 'Paul', 'English', start_date, 4.0)
+paul = Person(student, '1234 Chicago LN, Chicago, IL')
+print(paul)
+paul.student.change_major('BeingAwesome!')
+paul.student.update_gpa(3.0)
+print(paul)
+# clean up object
+del paul
--- Module10/class_definitions/student.py
+++ Module10/class_definitions/student.py
@@ -1,7 +1,10 @@
+from datetime import datetime
+
+
 class Student:
     """Student class"""
 
-    def __init__(self, lname, fname, major, gpa=''):
+    def __init__(self, lname, fname, major, startdate, gpa=''):
         """
         constructor to create a student
         :param lname: last name
@@ -30,6 +33,7 @@ class Student:
         self.last_name = lname
         self.first_name = fname
         self.major = major
+        self._start_date = startdate
         self.gpa = gpa
 
     def __str__(self):
@@ -37,4 +41,43 @@ class Student:
         prints student data
         :return: str of students data
         """
-        return self.last_name + ", " + self.first_name + " has major " + self.major + "with gpa: " + str(self.gpa)
+        return self.last_name + ", " + self.first_name + " has major " + self.major + \
+            "\nStart Date: " + str(self._start_date) + \
+            '\nGPA: ' + str(self.gpa)
+
+    def change_major(self, new_major):
+        """
+        changes the major of the student
+        checks if the new major is valid
+        :param new_major: new major to change.
+        """
+        name_characters = set("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!'- ' '")
+        if not (name_characters.issuperset(new_major)):
+            raise ValueError
+        self.major = new_major
+
+    def update_gpa(self, new_gpa):
+        """
+        updates the gpa of the student
+        :param new_gpa: new GPA
+        """
+        if not isinstance(new_gpa, float):
+            raise ValueError
+        if not 0 <= new_gpa <= 4:
+            raise ValueError
+        self.gpa = new_gpa
+
+    def display(self):
+        """
+        A better formatted version of student data
+        :return: str of student data
+        """
+        return "First Name: " + self.first_name + "\nLast Name:  " + self.last_name + \
+            "\nMajor: " + self.major + \
+            "\nStart Date: " + str(self._start_date) + \
+            '\nGPA: ' + str(self.gpa)
+
+# driver
+# start_date = datetime(2018, 9, 1)  # create a datetime to confirm
+# student = Student('Ford', 'Paul', 'English', start_date, 4.0)
+# print(student)
