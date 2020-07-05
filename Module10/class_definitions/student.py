961626b524e717185d063589b4d41efd38997613
applicationsbypaul <32409508+applicationsbypaul@users.noreply.github.com>
2020-07-05 05:45:40 -0500

    So found out why my program was failing. I didnt account for a space in the major and it was causing errors

--- Module10/class_definitions/student.py
+++ Module10/class_definitions/student.py
@@ -1,9 +1,14 @@
 class Student:
     """Student class"""
+
     def __init__(self, lname, fname, major, gpa=0.0):
-        name_characters = set("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'-")
+        name_characters = set("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'-' '")
         gpa_characters = set("1234567890.")
-        if not (name_characters.issuperset(lname) and name_characters.issuperset(fname)):
+        if not (name_characters.issuperset(lname)):
+            raise ValueError
+        if not (name_characters.issuperset(fname)):
+            raise ValueError
+        if not (name_characters.issuperset(major)):
             raise ValueError
         self.last_name = lname
         self.first_name = fname
@@ -12,3 +17,7 @@ class Student:
 
     def __str__(self):
         return self.last_name + ", " + self.first_name + " has major " + self.major + "with gpa: " + str(self.gpa)
+
+
+# driver
+s = Student('Ford', 'Paul', 'English')
